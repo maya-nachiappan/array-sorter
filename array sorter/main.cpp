a2bd2d4baedc1cf1ce2180458b6819ded98bcc59
maya-nachiappan <maya.n102304@gmail.com>
2024-10-14 01:49:42 -0700

    added comments and gitignore file

--- array sorter/main.cpp
+++ array sorter/main.cpp
@@ -9,7 +9,7 @@
 #include <algorithm> // for std::sort
 using namespace std;
 
-void sortArray(int arr[], int size) {
+void sortArray(int arr[], int size) { // creates function to sort array
     sort(arr, arr + size); // sort the array numerically
 }
 
@@ -26,23 +26,21 @@ int main() {
     // Ask the user to input the elements
     cout << "Enter " << size << " integers:" << endl;
     for (int i = 0; i < size; ++i) {
-        cin >> arr[i];
+        cin >> arr[i]; // integers are inputted in array
     }
 
     cout << "Original array: " << endl;
     for (int i = 0; i < size; ++i) {
-        cout << arr[i] << " ";
+        cout << arr[i] << " "; // prints original array
     }
     cout << endl;
 
-    sortArray(arr, size); // Sort the array
+    sortArray(arr, size); // Sorts the array
 
     cout << "Sorted array: " << endl;
     for (int i = 0; i < size; ++i) {
-        cout << arr[i] << " " << endl;
+        cout << arr[i] << " " << endl; // prints sorted array
     }
     cout << endl;
-
- //   delete[] arr; // Free the dynamically allocated memory
     return 0;
 }
